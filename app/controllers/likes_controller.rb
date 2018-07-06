354dd7d82477fd2e8975438d7d00e5a6c5faa485
Ka2K <cjswokdy@gmail.com>
2018-07-06 06:01:37 +0000

    maybe

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -12,7 +12,7 @@ class LikesController < ApplicationController
 
   def destroy
     @post_id = params[:post_id]
-    Like.find_by(user_id: current_user.id, post_id: @post_id.destroy)
+    Like.find_by(user_id: current_user.id, post_id: @post_id).destroy
     respond_to do |format|
       format.html { redirect_to :back }
       format.js {}
